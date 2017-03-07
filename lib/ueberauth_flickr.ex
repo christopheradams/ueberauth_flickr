fb839b6ce238a007c6691b3fc60ff42fcfe72663
Christopher Adams <christopher@fabricatorz.com>
2017-03-07 20:18:56 +0800

    Initial commit

---
+++ lib/ueberauth_flickr.ex
@@ -0,0 +1,18 @@
+defmodule UeberauthFlickr do
+  @moduledoc """
+  Documentation for UeberauthFlickr.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UeberauthFlickr.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ueberauth_flickr_test.exs
@@ -0,0 +1,8 @@
+defmodule UeberauthFlickrTest do
+  use ExUnit.Case
+  doctest UeberauthFlickr
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
